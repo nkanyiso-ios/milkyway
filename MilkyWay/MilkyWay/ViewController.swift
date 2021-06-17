bb18c4fa70db838a09ccfbb2bf24d8e6c8ce45ed
Nkanyiso <13.nkanyiso@gmail.com>
2021-06-17 16:46:12 +0200

    project creation

---
+++ MilkyWay/MilkyWay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilkyWay
+//
+//  Created by Nkanyiso Hlela on 2021/06/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
