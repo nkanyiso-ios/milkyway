39b15df7c44885e655cc8c367c1054178341b7ac
Nkanyiso <13.nkanyiso@gmail.com>
2021-06-17 16:52:44 +0200

    folder structure create

--- MilkyWay/MilkyWay/ViewController/HomeViewController.swift
+++ MilkyWay/MilkyWay/ViewController/HomeViewController.swift
@@ -1,13 +1,7 @@
-//
-//  ViewController.swift
-//  MilkyWay
-//
-//  Created by Nkanyiso Hlela on 2021/06/17.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
